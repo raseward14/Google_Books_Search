10f8c370cf595f5f31f38fbf96e54f88b476e46c
Richard Seward <raseward14@gmail.com>
2022-01-19 07:59:35 -0700

    built server.js added nodemon and tested a few route in postman, URL params, and POST JSON body

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// routes
+app.get('/hello', (req, res) => res.send('Hello!'));
+// post name as JSON BODY and get 'Hello Richard!'
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+// get name as URL PARAMS and get 'Hello Richard!'
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`))
+
+app.listen(8000, () => console.log('Listening on port 8000!'))
+
