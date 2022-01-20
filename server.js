8dcc151ec709e2a56bcf7dd622d03183b7af3303
Richard Seward <raseward14@gmail.com>
2022-01-20 08:05:33 -0700

    created a mongodb, tested google books get book and books api routes

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.get('/hello', (req, res) => res.send('Hello!'));
 // post name as JSON BODY and get 'Hello Richard!'
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 // get name as URL PARAMS and get 'Hello Richard!'
-app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`))
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
 
-app.listen(8000, () => console.log('Listening on port 8000!'))
+app.listen(8000, () => console.log('Listening on port 8000!'));
+
+// google books api documentation
+// https://developers.google.com/books/docs/v1/getting_started?csw=1
+// https://developers.google.com/books/docs/v1/using
 
