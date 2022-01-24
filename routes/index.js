feb8fe69718417b34afec3d82201ac26235046db
Richard Seward <raseward14@gmail.com>
2022-01-23 17:39:36 -0700

    created routes directory, added a get toread and post toread routes, post toread working

---
+++ routes/index.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const { Book } = require('../models');
+
+// middleware to use for all requests
+router.use((req, res, next) => {
+    // makes sure we got to the next routes and don't stop here
+    next();
+})
+
+// test route to see if our api works
+router.get('/', (req, res) => {
+    res.json({ message: 'welcome to our api :)' })
+});
+
+// routes that end in /toread (http://localhost:8000/api/toread)
+router.route('/toread')
+    .get((req, res) => {
+        // GET To Read Books List
+        Book.findAll((err, books) => {
+            if(err) {
+                res.send(err);
+            } else {
+                res.send(books);
+            };
+        });
+    })
+    .post((req, res) => {
+        // POST a Book To Read (http://localhost:8000/api/toread)
+        const newBook = new Book();
+        newBook.title = req.body.title;
+        newBook.authors.authorName = req.body.authors.authorName;
+        newBook.description = req.body.description;
+        // newBook.imageLink = req.body.imageLink;
+        // newBook.infoLink = req.body.infoLink;
+        // newBook.dateAdded = req.body.dateAdded;
+        newBook.save(() => {
+            if(err) {
+                res.send(err);
+            } else {
+                res.json({ message: 'Book just saved: ', newBook })
+            }
+        });
+    })
+
+
+// DELETE a Book To Read
+
+
+//routes that end in /alreadyread
+// GET books I've Read Shelf
+
+// POST Book I've Read
+
+// DELETE a Book I've Read
+
+
+// routes that end in /favorites
+// GET My Reading Recommendations
+
+// POST a My Reading Recommendation
+
+// DELETE a Reading Recommendation
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+// mongoose schema model
+const mongoose = require('mongoose');
+// routes
+const routes = require('./routes');
 
 // middleware
 app.use(express.urlencoded({extended: true}));
@@ -12,9 +16,22 @@ app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 // get name as URL PARAMS and get 'Hello Richard!'
 app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
 
+// all of our routes will be prefixed with /api
+app.use('/api', routes);
+
+mongoose.connect('mongodb://localhost:27017/google-books-search');
+
 app.listen(8000, () => console.log('Listening on port 8000!'));
 
 // google books api documentation
 // https://developers.google.com/books/docs/v1/getting_started?csw=1
 // https://developers.google.com/books/docs/v1/using
 
+
+// post a book
+// const { Book } = require('./models');
+// const myBook = new Book({
+//     title: 'Billy Summers'
+// })
+
+// myBook.save().then(() => console.log('the book im reading now!'))
