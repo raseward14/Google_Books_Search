7ebec2a97a0d592841da2294ed2c089d91f83698
Richard Seward <raseward14@gmail.com>
2022-01-26 08:03:33 -0700

    added get library books and post a library book routes to routews index.js

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.route('/read')
     .get((req, res) => {
         // GET To Read Books List
         Read.find((err, books) => {
-            if(err) {
+            if (err) {
                 // res.send(err);
             } else {
                 res.send(books);
@@ -26,44 +26,97 @@ router.route('/read')
     })
     .post((req, res) => {
         // POST a Book To Read (http://localhost:8000/api/read)
-        const newBook = new Read();
-        newBook.title = req.body.title;
-        newBook.authors.authorName = req.body.authors[0].authorName;
-        newBook.description = req.body.description;
-        // newBook.imageLink = req.body.imageLink;
-        // newBook.infoLink = req.body.infoLink;
-        // newBook.dateAdded = req.body.dateAdded;
-        newBook.save((err) => {
-            if(err) {
+        const newRead = new Read();
+        newRead.title = req.body.title;
+        newRead.authors.authorName = req.body.authors[0].authorName;
+        newRead.description = req.body.description;
+        // newRead.imageLink = req.body.imageLink;
+        // newRead.infoLink = req.body.infoLink;
+        // newRead.dateAdded = req.body.dateAdded;
+        newRead.save((err) => {
+            if (err) {
                 res.send(err);
             } else {
-                res.json({ message: 'Book just saved: ', newBook })
+                res.json({ message: 'Book just saved: ', newRead })
             }
         });
     });
 
 // routes that end in /read/:id (http://localhost:8000/api/read/:id)
 router.route('/read/:id')
-.get((req, res) => {
-    // GET a book I read
-    Read.findById(req.params.id, (err, book) => {
-        if(err) {
-            res.send(err);
-        } else {
-            res.send(book);
-        };
-    });
-});
-// DELETE a Book To Read
+    .get((req, res) => {
+        // GET a book I read
+        Read.findById(req.params.id, (err, book) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(book);
+            };
+        });
+    })
+    .delete((req, res) => {
+        // DELETE a Book To Read
+        Read.findByIdAndDelete(req.params.id, (err, book) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(book);
+            };
+        });
+
+    })
 
 
 
 //routes that end in /library
-// GET books I've Read Shelf
+router.route('/library')
+    .post((req, res) => {
+    // POST Book I've Read
+    const libraryBook = new Library();
+    libraryBook.title = req.body.title;
+    libraryBook.authors.authorName = req.body.authors.authorName
+    libraryBook.description = req.body.description
+        libraryBook.save(req.body, (err) => {
+            if(err) {
+                res.send(err);
+            } else {
+                res.send({ message: 'Book added to Library:', libraryBook });
+            };
+        })
+    })
+    .get((req, res) => {
+    // GET books I've Read Shelf
+        Library.find((err, books) => {
+            if(err) {
+                res.send(err);
+            } else {
+                res.send(books);
+            };
+        });
+    });
 
-// POST Book I've Read
-
-// DELETE a Book I've Read
+// routes that end in /library/:id
+router.route('/library/:id')
+    .get((req, res) => {
+        // GET a book I've read
+        Library.findById(req.params.id, (err, book) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(book);
+            };
+        })
+    })
+    .delete((req, res) => {
+        // DELETE a Book I've Read
+        Library.findByIdAndDelete(req.params.id, (err, book) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(book);
+            };
+        });
+    });
 
 
 // routes that end in /favorite
