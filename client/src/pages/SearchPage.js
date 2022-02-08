83e3021dea64ae6f3677c42ea319d0f0792f90ad
Richard Seward <raseward14@gmail.com>
2022-02-08 08:08:51 -0700

    can access array of books from search page

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -11,13 +11,24 @@ const SearchPage = () => {
         // clear the bookshelf each time
         bookShelf.innerHTML = null;
 
-        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
-        await setBooks(response.json());
-        console.log(books)
+        // fetch books using search, then convert to JSON, then for the books object, access only the .items property containing book data, and call async await function that will wait for this to finish 
+        const result = fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
+            .then((response) => response.json())
+            .then((books) => {
+                printResult();
+                return books.items;
+            });
+
+        // this function prints the booksArray
+        const printResult = async () => {
+            const booksArray = await result;
+            console.log(booksArray);
+        };
+
 
         for(let i = 0; i < books.length; i++) {
             var bookTitle = document.createElement('p');
-            bookTitle.textContent = null
+            // bookTitle.textContent = null
 
 
         }
