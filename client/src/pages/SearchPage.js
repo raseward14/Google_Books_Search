4f8b726061a99fc97e118c17aeda83f47ddcf6bb
Richard Seward <raseward14@gmail.com>
2022-02-25 08:03:43 -0700

    added console logs with numbers to figure out the order things are happenning to try to fix the null and undefined errors when writing to the DOM

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -3,83 +3,80 @@ import React, { useState, useEffect } from 'react';
 const SearchPage = () => {
     
     const [search, setSearch] = useState('');
+    const bookShelf = document.getElementById('container');
     const yourBooks = JSON.parse(localStorage.getItem('lastBookSearch'));
     const [books, setBooks] = useState(yourBooks || []);
-    const bookShelf = document.getElementById('container');
     
     useEffect(() => {
-        console.log(books);
+        console.log('three', books);
     }, [])
 
     const handleSubmit = () => {
         // clear the bookshelf each time
         bookShelf.innerHTML = null;
         console.log('handleSubmit')
-
-        // fetch books using search, then convert to JSON, then for the books object, access only the .items property containing book data, and call async await function that will wait for this to finish 
+        // fetch books using search, then convert to JSON, then for the books object, access only the .items property containing book data, and call async function that will wait for this to finish 
         const result = fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
             .then((response) => response.json())
             .then((books) => {
                 callResult();
                 return books.items;
             });
-
         // this function prints the booksArray
         const callResult = async () => {
             const booksArray = await result;
             setBooks(booksArray);
             localStorage.setItem('lastBookSearch', JSON.stringify(booksArray))
-            console.log(booksArray)
-            console.log(books)
             printResult(books);
         };
 
     };
 
-    // const addBook = (bookTitle, bookAuthors, bookDescription, bookImage, bookLink) => {
-    //     const item = {
-    //         title: `${bookTitle}`,
-    //         authors: `${bookAuthors}`,
-    //         description: `${bookDescription}`,
-    //         image: bookImage,
-    //         link: bookLink
-    //     }
-    //     setBooks([ {...item} ])
-    // }
+    document.addEventListener('DOMContentLoaded', function(event) {
+        console.log('one', 'book state', books)
+        printResult(books)
+    })
 
     const printResult = (booksArray) => {
+        console.log('two', booksArray.length)
 
-        console.log(booksArray)
+        setTimeout((booksArray) => {
+            if(undefined !== booksArray && booksArray.length) {
 
-        for (let i = 0; i < booksArray.length; i++) {
-            var singleBook = document.createElement('div');
-            singleBook.classList.add('book')
-            var bookTitle = document.createElement('p');
-            bookTitle.textContent = booksArray[i]?.volumeInfo?.title;
-            var bookAuthors = document.createElement('p');
-            bookAuthors.textContent = booksArray[i]?.volumeInfo?.authors;
-            var bookDescription = document.createElement('p');
-            bookDescription.textContent = booksArray[i]?.volumeInfo?.description;
-            bookDescription.classList.add('book-content');
-            var bookImage = document.createElement('img');
-            bookImage.src = `${booksArray[i]?.volumeInfo?.imageLinks?.thumbnail}`;
-            bookImage.classList.add('book-content');
-
-            var bookLink = document.createElement('a');
-            bookLink.href = `${booksArray[i]?.volumeInfo?.previewLink}`;
-            bookLink.textContent = 'Learn more!'
-
-            singleBook.append(bookTitle);
-            singleBook.append(bookAuthors);
-            singleBook.append(bookImage);
-            singleBook.append(bookDescription);
-            singleBook.append(bookLink);
-
-            bookShelf.append(singleBook);
-        }
+                for (let i = 0; i < booksArray.length; i++) {
+                    var singleBook = document.createElement('div');
+                    singleBook.classList.add('book')
+                    var bookTitle = document.createElement('p');
+                    bookTitle.textContent = booksArray[i]?.volumeInfo?.title;
+                    var bookAuthors = document.createElement('p');
+                    bookAuthors.textContent = booksArray[i]?.volumeInfo?.authors;
+                    var bookDescription = document.createElement('p');
+                    bookDescription.textContent = booksArray[i]?.volumeInfo?.description;
+                    bookDescription.classList.add('book-content');
+                    var bookImage = document.createElement('img');
+                    bookImage.src = `${booksArray[i]?.volumeInfo?.imageLinks?.thumbnail}`;
+                    bookImage.classList.add('book-content');
+                    
+                    var bookLink = document.createElement('a');
+                    bookLink.href = `${booksArray[i]?.volumeInfo?.previewLink}`;
+                    bookLink.textContent = 'Learn more!'
+                    
+                    singleBook.append(bookTitle);
+                    singleBook.append(bookAuthors);
+                    singleBook.append(bookImage);
+                    singleBook.append(bookDescription);
+                    singleBook.append(bookLink);
+                    
+                    bookShelf.append(singleBook);
+                }
+            } else {
+                console.log('this is to prevent an error');
+                return
+            }
+        }, 5000)
     };
 
-    printResult(books);
+    
 
     return (
         <div>
