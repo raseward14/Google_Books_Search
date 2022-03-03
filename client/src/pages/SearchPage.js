d410f1baaa7b0d53308fa8e525f900a2a3bb37d5
Richard Seward <raseward14@gmail.com>
2022-03-03 07:54:08 -0700

    moved clearing bookshelf to useEffect to only clear when books hook changes dropped handleSubmit below print books function, removed the call to print books from handle submit, now it only lives in the useEffect

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -8,13 +8,69 @@ const SearchPage = () => {
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
-        console.log('three', books);
+        console.log('use effect fired books:', books);
+        bookShelf.innerHTML = null;
         printResult(books)
     }, [books])
 
+
+    
+    const printResult = (booksArray) => {
+        console.log('printing this many books:', booksArray.length)
+        
+        for (let i = 0; i < booksArray.length; i++) {
+            
+            var singleBook = document.createElement('div');
+            singleBook.classList.add('single-book');
+            
+            var lineBreak = document.createElement('br');
+            
+            var bookTitle = document.createElement('p');
+            bookTitle.textContent = booksArray[i]?.volumeInfo?.title;
+            
+            var bookAuthors = document.createElement('p');
+            bookAuthors.textContent = booksArray[i]?.volumeInfo?.authors;
+            
+            var bookDescription = document.createElement('p');
+            bookDescription.textContent = booksArray[i]?.volumeInfo?.description;
+            bookDescription.classList.add('book-content');
+            
+            var bookImage = document.createElement('img');
+            bookImage.src = `${booksArray[i]?.volumeInfo?.imageLinks?.thumbnail}`;
+            bookImage.classList.add('book-content');
+            
+            var myLibrary = document.createElement('button');
+            myLibrary.textContent = 'Add to Library';
+            
+            var contentContainer = document.createElement('div');
+            contentContainer.classList.add('contentContainer');
+            
+            var headingContainer = document.createElement('div');
+            headingContainer.classList.add('headingContainer');
+            
+            var bookLink = document.createElement('a');
+            bookLink.href = `${booksArray[i]?.volumeInfo?.previewLink}`;
+            bookLink.textContent = 'Buy me!'
+            
+            headingContainer.append(bookTitle);
+            bookTitle.append(lineBreak);
+            bookTitle.append(bookAuthors);
+            bookAuthors.append(lineBreak);
+            bookAuthors.append(bookLink);
+            singleBook.append(headingContainer);
+            
+            contentContainer.append(bookImage);
+            contentContainer.append(bookDescription);
+            singleBook.append(contentContainer);
+            
+            
+            const bookShelf = document.getElementById('container');
+            bookShelf.append(singleBook);
+        }
+    };
+
     const handleSubmit = () => {
         // clear the bookshelf each time
-        bookShelf.innerHTML = null;
         console.log('handleSubmit')
         // fetch books using search, then convert to JSON, then for the books object, access only the .items property containing book data, and call async function that will wait for this to finish 
         const result = fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
@@ -28,74 +84,18 @@ const SearchPage = () => {
             const booksArray = await result;
             setBooks(booksArray);
             localStorage.setItem('lastBookSearch', JSON.stringify(booksArray))
-            printResult(booksArray);
         };
-
+    };
+    
+    const loadHistory = async () => {
+        const yourBooks = await JSON.parse(localStorage.getItem('lastBookSearch'));
+        printResult(yourBooks);
     };
 
-    const printResult = (booksArray) => {
-        console.log('two', booksArray.length)
+    loadHistory();
 
-        for (let i = 0; i < booksArray.length; i++) {
-
-            var singleBook = document.createElement('div');
-            singleBook.classList.add('single-book');
-
-            var lineBreak = document.createElement('br');
-
-            var bookTitle = document.createElement('p');
-            bookTitle.textContent = booksArray[i]?.volumeInfo?.title;
-
-            var bookAuthors = document.createElement('p');
-            bookAuthors.textContent = booksArray[i]?.volumeInfo?.authors;
-
-            var bookDescription = document.createElement('p');
-            bookDescription.textContent = booksArray[i]?.volumeInfo?.description;
-            bookDescription.classList.add('book-content');
-
-            var bookImage = document.createElement('img');
-            bookImage.src = `${booksArray[i]?.volumeInfo?.imageLinks?.thumbnail}`;
-            bookImage.classList.add('book-content');
-
-            var myLibrary = document.createElement('button');
-            myLibrary.textContent = 'Add to Library';
-
-            var contentContainer = document.createElement('div');
-            contentContainer.classList.add('contentContainer');
-
-            var headingContainer = document.createElement('div');
-            headingContainer.classList.add('headingContainer');
-
-            var bookLink = document.createElement('a');
-            bookLink.href = `${booksArray[i]?.volumeInfo?.previewLink}`;
-            bookLink.textContent = 'Buy me!'
-
-            headingContainer.append(bookTitle);
-            bookTitle.append(lineBreak);
-            bookTitle.append(bookAuthors);
-            bookAuthors.append(lineBreak);
-            bookAuthors.append(bookLink);
-            singleBook.append(headingContainer);
-
-            contentContainer.append(bookImage);
-            contentContainer.append(bookDescription);
-            singleBook.append(contentContainer);
-
-            
-            const bookShelf = document.getElementById('container');
-            bookShelf.append(singleBook);
-        }
-    };
-
-const loadHistory = async () => {
-    const yourBooks = await JSON.parse(localStorage.getItem('lastBookSearch'));
-    printResult(yourBooks);
-};
-
-loadHistory();
-
-return (
-    <div>
+    return (
+        <div>
             <p>Search</p>
             <input placeholder='Search'
                 onChange={(event) => {
