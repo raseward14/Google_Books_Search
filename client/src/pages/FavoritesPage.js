cfe94dce46d2f937538c94ff7288d1493c1c19a5
Richard Seward <raseward14@gmail.com>
2022-03-15 08:44:15 -0600

    stuck in an infinite loop of retrieving favorites.. need to figure it out

--- client/src/pages/FavoritesPage.js
+++ client/src/pages/FavoritesPage.js
@@ -5,18 +5,34 @@ const FavoritesPage = () => {
 
     const [favorites, setFavorites] = useState([]);
     const bookShelf = document.getElementById('bookshelf');
+    let APIFavorites;
 
     useEffect(() => {
         // async function loadFavorites() {
             //     let result = await favoriteAPIFunctions.getFavorites();
             //     let favoritesArray = result.data;
-            //     setFavorites(favoritesArray);
-            if(favorites.length === 0) {
-                console.log('favorites is empty')
-                return
-            } else {
-                console.log('favorites have been set!', favorites)
-            }
+            //     setFavorites(favoritesArray);    
+
+        console.log(typeof favorites)
+        if(typeof favorites === 'object') {
+            console.log('object:', favorites);
+        } else {
+            console.log('array:', favorites);
+            // printFavorites(favorites)
+        }
+
+
+
+
+            // if(favorites.length === 0) {
+            //     console.log('favorites is empty')
+            //     return
+            // } else {
+            //     console.log('favorites have been set!', favorites)
+            // }
+
+
+
             // printFavorites(favoritesArray)
             // console.log('favorites array: ', favoritesArray)
             // console.log('favorites: ', favorites)
@@ -34,7 +50,7 @@ const FavoritesPage = () => {
         //     printFavorites(favorites)
         // })
         // .catch((err) => console.log(err))
-    }, []);
+    }, [favorites]);
 
     const printFavorites = (favoritesArray) => {
         for (let i = 0; i < favoritesArray.length; i++) {
@@ -45,7 +61,6 @@ const FavoritesPage = () => {
             }
             
             var singleBook = document.createElement('div');
-            var lineBreak = document.createElement('br');
             
             var headingContainer = document.createElement('div');
 
@@ -55,74 +70,54 @@ const FavoritesPage = () => {
             var bookLink = document.createElement('a');
 
             var buttonContainer = document.createElement('div');
-            // var favButton = document.createElement('button');
-            // var libraryButton = document.createElement('button');
-            // var toReadButton = document.createElement('button');
 
-            bookTitle.textContent = favoritesArray[i]?.volumeInfo?.title;
-            bookAuthors.textContent = favoritesArray[i]?.volumeInfo?.authors;
-            bookLink.href = `${favoritesArray[i]?.volumeInfo?.previewLink}`;
-            bookLink.textContent = 'Buy me!'
-            // favButton.textContent = 'FAVORITE';
-            // libraryButton.textContent = 'ADD TO YOUR LIBRARY';
-            // toReadButton.textContent = 'NEED TO READ';
-            
-            // favButton.addEventListener('click', favoriteBook, once);
-            // libraryButton.addEventListener('click', addToLibrary, once);
-            // toReadButton.addEventListener('click', addToRead, once);
-           
+            bookTitle.textContent = favoritesArray[i]?.title;
+            // bookAuthors.textContent = favoritesArray[i]?.volumeInfo?.authors;
+            // bookLink.href = `${favoritesArray[i]?.volumeInfo?.previewLink}`;
+            // bookLink.textContent = 'Buy me!'
+                       
             headingContainer.classList.add('headingContainer');
             
             summaryContainer.append(bookTitle);
-            summaryContainer.append(bookAuthors)
-            summaryContainer.append(bookLink);
-
-            // buttonContainer.append(favButton);
-            // buttonContainer.append(libraryButton);
-            // buttonContainer.append(toReadButton);
+            // summaryContainer.append(bookAuthors)
+            // summaryContainer.append(bookLink);
 
             headingContainer.append(summaryContainer);
-            headingContainer.append(buttonContainer);
 
             singleBook.append(headingContainer);
             
             var contentContainer = document.createElement('div');
             var bookDescription = document.createElement('p');
-            var bookImage = document.createElement('img');
+            // var bookImage = document.createElement('img');
             
-            bookDescription.textContent = favoritesArray[i]?.volumeInfo?.description;
-            bookImage.src = `${favoritesArray[i]?.volumeInfo?.imageLinks?.thumbnail}`;
+            bookDescription.textContent = favoritesArray[i]?.description;
+            // bookImage.src = `${favoritesArray[i]?.volumeInfo?.imageLinks?.thumbnail}`;
 
-            buttonContainer.classList.add('button-container')
             contentContainer.classList.add('content-container');
             bookDescription.classList.add('book-content');
-            bookImage.classList.add('book-content');
-            bookLink.classList.add('book-link')
+            // bookImage.classList.add('book-content');
+            // bookLink.classList.add('book-link')
  
             singleBook.classList.add('single-book');       
             
-            contentContainer.append(bookImage);
+            // contentContainer.append(bookImage);
             contentContainer.append(bookDescription);
             singleBook.append(contentContainer);
             
-            // const bookShelf = document.getElementById('container');
+            const bookShelf = document.getElementById('container');
             bookShelf.append(singleBook);
         }
     };
 
     async function loadFavorites() {
         let result = await favoriteAPIFunctions.getFavorites();
-        let favoritesArray = result.data;
-        // if(bookShelf.innerHTML !== null) {
-            // bookShelf.innerHTML = null;
-        // }
-        console.log(favoritesArray)
-        setFavorites(favoritesArray);
-        // printFavorites(favoritesArray);
+        APIFavorites = result.data;
+        console.log(APIFavorites)
+        setFavorites(APIFavorites);
     };
-
+    
     // loadFavorites();
-
+    
     return (
         <div>
             <p>Favorites</p>
