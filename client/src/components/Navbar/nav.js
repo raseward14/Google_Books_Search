423820a573fbfffbca5f449f93bd69a855b2b0ce
Richard Seward <raseward14@gmail.com>
2022-03-27 14:20:23 -0600

    built read page, updated nav bar

--- client/src/components/Navbar/nav.js
+++ client/src/components/Navbar/nav.js
@@ -6,9 +6,9 @@ const NavBar = () => {
     return (
         <ul className='navlinks'>
             <Link to='/' active className='navlink'>Home</Link>
-            <Link to='search' className='navlink'>Search</Link>
-            <Link to='read' className='navlink'>Read</Link>
-            <Link to='library' className='navlink'>Library</Link>
+            <Link to='search' className='navlink'>Browse</Link>
+            <Link to='read' className='navlink'>Want to Read</Link>
+            <Link to='library' className='navlink'>Read</Link>
             <Link to='favorites' className='navlink'>Favorites</Link>
         </ul>
     );
--- client/src/pages/LibraryPage.js
+++ client/src/pages/LibraryPage.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as libraryAPIFunctions from '../utils/LibraryAPI';
+
 const LibraryPage = () => {
+
+    const [library, setLibrary] = useState([]);
+    let APILibrary;
+
+    async function loadLibrary() {
+        let result = await libraryAPIFunctions.getLibrary();
+        APILibrary = result.data;
+        setLibrary(APILibrary);
+    };
+
+    useEffect(() => {
+        loadLibrary();
+    }, [])
+
+    // i can favorite books from my library DEV-298
+
     return (
-        <>
-            <p>Library of books i've read.</p>
-        </>
+        <div>
+            <p>Library of books I've read.</p>
+            <div>
+                {library.map((book) => (
+                    <div key={book.id} className='single-book'>
+                        <div className='heading-container'>
+                            <div>
+                                <p>{book.title}</p>
+                                <p>{book.authors}</p>
+                                <a href={book.infoLink} className='book-link'>Buy me!</a>
+                            </div>
+                        </div>
+                        <div className='content-container'>
+                            <img src={book.imageLink} className='book-content' />
+                            <p className='book-content'>{book.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
 export default LibraryPage;
