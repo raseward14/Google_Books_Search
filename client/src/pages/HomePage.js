637c47edc9b7d95011c169d5f77013a7927ef0e2
Richard Seward <raseward14@gmail.com>
2022-05-18 07:57:47 -0600

    updated the homePage to describe what can be done with this app!

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,13 +4,13 @@ const HomePage = () => {
         <>
             <h2>Welcome</h2>
             <p>
-                Search for a book, then save it! Add books you'd like to read to the <em>Read</em> list.
+                Search for a book, then save it! Add books you'd like to read to the <em>Want to Read</em> list, and books you've already read to the <em>Read list</em>.
             </p>
             <p>
-                Add books you've already read to like to your <em>Library</em> list.
+                Move books you've already read from the <em>Want to Read List</em>, to the <em>Read List</em>.  
             </p>
             <p>
-                Add your Favorites to the <em>Favorites</em> list.
+                Add your Favorites from the <em>Read List</em> to the <em>Favorites</em> list.
             </p>
 
         </>
