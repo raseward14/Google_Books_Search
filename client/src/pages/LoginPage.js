0562053af4e5628f0928ddf618e40c75c031b00d
Richard Seward <raseward14@gmail.com>
2022-07-12 07:41:20 -0600

    login and register forms complete

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,10 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from '../context/AuthProvider';
 import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
+    // if we successfully authenticate when we log in, we will set our new Auth state, and store it in the global context
+    const { setAuth } = useContext(AuthContext);
     // set focus on first input when the component loads
     const userRef = useRef();
     // will need to set focus on the errors, especially for a screen reader to read
@@ -29,10 +32,35 @@ const LoginPage = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         // implement axios, and global state for auth - store authentication
-        console.log(user, pwd);
-        setUser('');
-        setPwd('');
-        setSuccess(true);
+
+
+        // try {
+        //     const response = await axios.post(LOGIN_URL, 
+        //         JSON.stringify({user, pwd}),
+        //         {
+        //             headers: { 'Content-type': 'application/json'},
+        //             withCredentials: true
+        //         }
+        //     )
+        //     console.log(JSON.stringify(response?.data));
+        //     const accessToken = response?.data?.accessToken;
+        //     const roles = response?.data?.roles;
+        // setAuth({ user, pwd, roles, accessToken });
+            setUser('');
+            setPwd('');
+            setSuccess(true);
+        // } catch (err) {
+        //     if(!err?.response) {
+        //         setErrMsg('No Server Response');
+        //     } else if (err?.response?.status === 400) {
+        //         setErrMsg('Missing Username or Password');
+        //     } else if (err.response?.status === 401) {
+        //         setErrMsg('Unauthorized');
+        //     } else {
+        //         setErrMsg('Login Failed');
+        //     }
+        //     errRef.current.focus();
+        // }
     }
 
 
