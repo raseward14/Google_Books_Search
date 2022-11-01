adbd3032d580827ecb76d5f2f09d5a743e5bcfbb
Richard Seward <raseward14@gmail.com>
2022-11-01 17:50:22 -0600

    started useRefreshToken hook

---
+++ client/src/hooks/useRefreshToken.js
@@ -0,0 +1,22 @@
+import axios from '../api/axios';
+import useAuth from './useAuth';
+
+const useRefreshToken = () => {
+    const { setAuth } = useAuth();
+
+    const refresh = async () => {
+        const response = await axios.get('/refresh', {
+            withCredentials: true
+        });
+        setAuth(prev => {
+            console.log(JSON.stringify(prev));
+            console.log(response.data.accessToken);
+            return { ...prev, accessToken: response.data.accessToken }
+        });
+        // will call this function when our initial request fails, when our accessToken expires, will refresh, get a new token
+        return response.data.accessToken;
+    }
+    return <div></div>
+};
+
+export default useRefreshToken;
