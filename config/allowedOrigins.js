3b84244676762e278d7f162363768099ac1dd30f
richardseward <raseward14@gmail.com>
2023-01-24 18:24:36 -0700

    ran run build for react app

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'https://blurb-books.herokuapp.com/',
     'http://localhost:3000',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:3000'
 ];
 
 module.exports = allowedOrigins
