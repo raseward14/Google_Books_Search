1975a560d01093da625144b3f12766ec9dda10ca
richardseward <raseward14@gmail.com>
2023-01-28 20:40:23 -0700

    flipped axios.js back to process.env.REACT_APP_BASE_URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// let BASE_URL;
+let BASE_URL;
 
-// if (process.env.NODE_ENV === "production") {
-//     BASE_URL = process.env.REACT_APP_BASE_URL
-// } else {
-//     BASE_URL = 'http://localhost:3000'
-// }
+if (process.env.NODE_ENV === "production") {
+    BASE_URL = process.env.REACT_APP_BASE_URL
+} else {
+    BASE_URL = 'http://localhost:3000'
+}
 
-const BASE_URL = process.env.production.HOST || 'http://localhost:3000'
+// const BASE_URL = process.env.production.HOST || 'http://localhost:3000'
 
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
