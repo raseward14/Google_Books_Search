939065b8499cc9829be9df2aca239fcb433341ef
richardseward <raseward14@gmail.com>
2023-01-30 17:45:48 -0700

    removed request headers from postLogin

--- client/src/utils/AuthAPI.js
+++ client/src/utils/AuthAPI.js
@@ -3,16 +3,18 @@ import axios from 'axios';
 // POST login
 const postLogin = (userInfo) => {
 
-        const axiosConfig = {
-                withCredentials: true,
-                credentials: 'include',
-                headers: {
-                  Accept: 'application/json',
-                  'Content-Type': 'application/json'
-                }
-              };
+        // const axiosConfig = {
+        //         withCredentials: true,
+        //         credentials: 'include',
+        //         headers: {
+        //           Accept: 'application/json',
+        //           'Content-Type': 'application/json'
+        //         }
+        //       };
 
-        return axios.post('/api/auth', userInfo, axiosConfig);        
+        // return axios.post('/api/auth', userInfo, axiosConfig);        
+        return axios.post('/api/auth', userInfo);        
+
 };
     
 export { postLogin };
