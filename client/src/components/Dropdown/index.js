1e97efb2de3040a23d07634ecd51d7468a119f6b
Richard Seward <raseward14@gmail.com>
2023-09-06 18:29:10 -0600

    dynamically render dates on page load with font awesome xmark icons that float right and highlight on hover

--- client/src/components/Dropdown/index.js
+++ client/src/components/Dropdown/index.js
@@ -1,78 +1,76 @@
 import React, { useState, useEffect } from "react";
+// fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { faThumbtack } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+
 import './style.css';
 
 
 const Dropdown = ({ datesRead, index }) => {
     const [arrow, setArrow] = useState(false);
     const [datesReadArray, setDatesReadArray] = useState();
-    const dropdownParent = document.getElementById(`${index}`);
-
-    // const createDropdownElements = (array) => {
-    //     Object.keys(array).forEach(date => {
-    //         let dateString = JSON.stringify(date)
-    //         let dateDiv = document.createElement('div');
-    //         dateDiv.textContent(dateString);
-    //         dropdownParent.append(dateDiv);
-    //     })
-    // }
+    const [itemIndex, setItemIndex] = useState();
 
     const flipArrow = () => {
         if (arrow) {
+            console.log(itemIndex)
+            document.getElementById(`${itemIndex}`).classList.toggle("show-dates")
             setArrow(false);
         } else {
+            console.log(itemIndex)
+            document.getElementById(`${itemIndex}`).classList.toggle("show-dates")
             setArrow(true)
         }
     }
 
-    // useEffect(() => {
-    //     if(datesReadArray !== undefined) {
-    //         createDropdownElements(datesReadArray)
-    //     }
-    // }, [datesReadArray]);
-
-    // useEffect(() => {
-    //     setDatesReadArray(datesRead);
-    // });
-
     useEffect(() => {
-        console.log('dropdown got this index: ', index)
-    })
-
-    useEffect(() => {
-        console.log(`our existing dates array for ${index}: ${datesReadArray}`)
-    }, [datesReadArray])
-
-    useEffect(() => {
-        if(datesRead !== undefined) {
-            setDatesReadArray(datesRead)
+        if (index !== undefined) {
+            setItemIndex(index)
         }
-    })
+    });
+
+    useEffect(() => {
+        if (datesRead !== undefined) {
+            let dateArray = datesRead[0].split(', ');
+            setDatesReadArray(dateArray);
+        }
+    }, [itemIndex])
 
     return (
         <div className="flatpickr-container">
             {arrow ?
-            <>
-                <button
-                    className="date-arrow"
-                    onClick={() => {
-                        flipArrow();
-                    }}>Dates Read
-                    <i className="arrow up date-arrow" />
-                </button>
-                <div id={`${index}`}>
-                    <div>date1</div>
-                    <div>date2</div>
-                    <div>date3</div>
+                <div className="date-dropdown">
+                    <button
+                        className="date-arrow"
+                        onClick={() => {
+                            flipArrow();
+                        }}>Dates Read
+                        <i className="arrow up date-arrow" />
+                    </button>
+                    <div id={`${itemIndex}`} className='date-dropdown-content'>
+                        {datesReadArray.map((date) => (
+                            <div key={date[index]}>{date}
+                            <FontAwesomeIcon
+                            className="remove-date-icon"
+                            icon={icon({ name: "rectangle-xmark", style: "regular" })}/>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </>
                 :
-                <button
-                    className="date-arrow"
-                    onClick={() => {
-                        flipArrow();
-                    }}>Dates Read
-                    <i className="arrow down date-arrow" />
-                </button>
+                <div>
+                    <button
+                        className="date-arrow"
+                        onClick={() => {
+                            flipArrow();
+                        }}>Dates Read
+                        <i className="arrow down date-arrow" />
+                    </button>
+                    <div id={`${itemIndex}`}></div>
+                </div>
             }
         </div>
     );
