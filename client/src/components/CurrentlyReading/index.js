70229eccb71cb3dc73ec92bacf36633dac72d9da
Richard Seward <raseward14@gmail.com>
2023-11-02 18:34:35 -0600

    create function to update in progress property for book in the db

--- client/src/components/CurrentlyReading/index.js
+++ client/src/components/CurrentlyReading/index.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import './style.css';
-import useAxiosPrivate from '../../hooks/useAxiosPrivate'
+// CSS file for react tooltip
+import 'react-tooltip/dist/react-tooltip.css'
 
+import useAxiosPrivate from '../../hooks/useAxiosPrivate'
 import * as wantAPIFunctions from '../../utils/WantToReadAPI';
+import { Tooltip as ReactTooltip } from "react-tooltip";
+
 
 
 const CurrentlyReading = () => {
@@ -11,6 +15,13 @@ const CurrentlyReading = () => {
     const userID = sessionStorage.getItem("userID");
     // state variable to contain whats currently being read
     const [inProgress, setInProgress] = useState([]);
+
+    async function removeFromInProgress(id) {
+        // PUT needs, axiosPrivate, id, body, accessToken
+        await wantAPIFunctions.updateWantToRead(axiosPrivate, id, {
+            "inProgress": false
+        }, accessToken)
+    };
 
     async function loadInProgress() {
         // GET want to read - needs: axiosPrivate, accessToken and userID
@@ -43,6 +54,15 @@ const CurrentlyReading = () => {
                     key={index}>
                         <p style={{ color: "white" }}>{book.title}</p>
                         <img src={book.imageLink} className="fade"></img>
+                        <ReactTooltip id="removeFromIP" />
+
+                        <span
+                                                    data-tooltip-id="removeFromIP"
+                                                    data-tooltip-content="Remove from in progress."
+                                                    className="book-modal-close ip-x"
+                                                    onClick={() => {
+                                                        removeFromInProgress(book._id);
+                                                    }}>&times;</span>
                     </div>
                 ))}
             </div>
